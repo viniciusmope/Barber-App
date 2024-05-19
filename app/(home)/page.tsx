5b9f8781817de6d98e8f7780f2050a043ddf3a06
Vinicius Moreira <viniciuspemo1@gmail.com>
2024-05-19 11:59:49 -0300

    feat: add footer component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -15,7 +15,7 @@ export default async function Home() {
       <Header />
 
       <div className="px-5 pt-5">
-        <h2 className="text-xl font-bold">Olá, Miguel!</h2>
+        <h2 className="text-xl font-bold">Olá!</h2>
         <p className="capitalize text-sm">
           {format(new Date(), "EEEE',' dd 'de' MMMM", {
             locale: ptBR,
@@ -34,6 +34,16 @@ export default async function Home() {
 
       <div className="mt-6">
         <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">Recomendados</h2>
+
+        <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {barbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
+      </div>
+
+      <div className="mt-6 mb-[4.5rem]">
+        <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">Populares</h2>
 
         <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
           {barbershops.map((barbershop) => (
---
+++ app/_components/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+      <div className="w-full bg-secondary py-6 px-5">
+        <p className="text-gray-400 text-xs font-bold opacity-75">© Desenvolvido por Vinícius Moreira</p>
+      </div>
+    );
+  };
+  
+  export default Footer;
