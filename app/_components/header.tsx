9a368c9e8cf7dfcb839e76c43bd8f7b322239016
Vinicius Moreira <viniciuspemo1@gmail.com>
2024-05-19 11:38:24 -0300

    feat: add initial booking component

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <Card>
       <CardContent className="p-5 justify-between items-center flex flex-row">
-        <Image src="/logo.png" alt="FSW Barber" height={18} width={120} />
+        <Image src="/logo1.png" alt="FSW Barber" height={18} width={120} />
         <Button variant="outline" size="icon" className="h-8 w-8">
           <MenuIcon size={16} />
         </Button>
